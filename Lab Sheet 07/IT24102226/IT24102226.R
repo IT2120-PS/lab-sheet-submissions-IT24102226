951ec7a43f9c52fd0d29fb553d2bdddc68f38911
IT24102226 <it24102226@my.sliit.lk>
2025-09-15 22:30:31 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102226/IT24102226.R
@@ -0,0 +1,19 @@
+setwd("C:\\Users\\Dell\\Desktop\\IT24102226")
+
+#Exercise
+#1
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+#2
+pexp(2, rate=1/3)
+
+#3
+mean <- 100
+sd <- 15
+
+#i
+1 - pnorm(130, mean=mean, sd = sd)
+
+#ii
+qnorm(0.95, mean=mean, sd=sd)
+
